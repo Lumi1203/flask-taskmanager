31fedd01c939faaa4a9cbe43c0ef959c4f0ea10d
Olumide Olumoyin <engrlumynem@gmail.com>
2025-07-08 20:18:27 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = ["Open Centre", "Check classes for hazard","Clock in for work"]
+
+@app.route("/")
+@app.route("/index")
+def index():
+  return render_template("index.html")
+@app.route("/tasks")
+def task_list():
+  return render_template("tasks.html", tasks=tasks)
+
+#@app.route("/addtask", methods=["GET", "POST"])
+#def add_task():
+#  if request.method == "POST":
+#    task = request.form.get("task")
+#    tasks.append(task)
+#    print(tasks)
+#    return redirect(url_for("task_list"))
+#  return render_template("addtask.html")
+
+@app.route("/addtask", methods=["GET"])
+def add_task_get():
+  return render_template("addtask.html")
+
+@app.route("/addtask", methods=["POST"])
+def add_task():
+  task = request.form.get("task")
+  tasks.append(task)
+  print(tasks)
+  return redirect(url_for("task_list"))
+
+@app.route("/deletetask/<int:task_id>")
+def delete_task(task_id):
+  if 0 <= task_id <= len(tasks):
+    tasks.pop(task_id)
+  return redirect(url_for("task_list"))
